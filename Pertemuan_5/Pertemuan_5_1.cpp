3c967cf2e8191849c840746539920ff9b4964f39
KevinFebriano <alfahdad10@gmail.com>
2024-10-14 13:33:19 +0700

    Update Pertemuan_5_1.cpp

--- Pertemuan_5/Pertemuan_5_1.cpp
+++ Pertemuan_5/Pertemuan_5_1.cpp
@@ -22,3 +22,26 @@ void fizzbuzz(){
     }
     return 0;
 }
+void reverse(){
+vector<int> reverseVector(const vector<int>& h){
+    vector<int> r(h.rbegin(), h.rend());
+    return r;
+}
+    int n;
+    cout<<"Masukkan Jumlah Angka: ";
+    cin>>n;
+
+    vector<int> h(n);
+    cout<<"Masukkan "<<n<<" Angka: ";
+    for(int i=0; i<n; ++i){
+        cin>>h[i];
+    }
+    vector<int> r=reverseVector(h);
+
+    cout<<"Reversed: ";
+    for(int i=0; i<r.size(); ++i){
+        cout<<r[i]<<" ";
+    }
+    cout<<endl;
+    return 0;
+}
