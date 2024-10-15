c6718686c25c29c4a67c6cc163f99faf36c52e1b
KevinFebriano <alfahdad10@gmail.com>
2024-10-15 09:50:47 +0700

    Add files via upload

--- Pertemuan_5/Pertemuan_5.cpp
+++ Pertemuan_5/Pertemuan_5.cpp
@@ -2,24 +2,53 @@
 #include<vector>
 using namespace std;
 
-int main(){
+void fizzbuzz(){
     int n; cin>>n;
-    vector<string> h;
-    h.reserve(n);
+    vector<string>hasil;
+    hasil.reserve(n);
 
     for(int i=1; i<=n; ++i){
         if(i%3 == 0 && i%5 == 0){
-            h.push_back("FizzBuzz");
-        }else if(i%3 == 0){
-            h.push_back("Fizz");
-        }else if(i%5 == 0){
-            h.push_back("Buzz");
-        }else{
-            h.push_back(to_string(i));
+            hasil.push_back("FizzBuzz");
+        } else if (i % 3 == 0){
+            hasil.push_back("Fizz");
+        } else if (i % 5 == 0){
+            hasil.push_back("Buzz");
+        } else{
+            hasil.push_back(to_string(i));
         }
     }
-    for(const auto & h : h){
-        cout<<h<<endl;
+    for(const auto & hasil : hasil){
+        cout<<hasil<<endl;
     }
-    return 0;
+}
+
+vector<int> reverseVector(const vector<int>& vec){
+    vector<int> reversedVec(vec.rbegin(), vec.rend());
+    return reversedVec;
+}
+
+void reverse(){
+    int n;
+    cout<<"Masukkan Jumlah Angka: ";
+    cin>>n;
+
+    vector<int>angka(n);
+    cout<<"Masukkan "<<n<<" Angka: ";
+    for(int i=0; i<n; ++i){
+        cin>>angka[i];
+    }
+
+    vector<int> reversedNumbers = reverseVector(angka);
+
+    cout<<"Reversed: ";
+    for(int i=0; i<reversedNumbers.size(); ++i){
+        cout<<reversedNumbers[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    fizzbuzz();
+    reverse();
 }
